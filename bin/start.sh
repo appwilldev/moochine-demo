c5a5da777b575e03909f126c09a51477f02618c1
Demiao Lin <ldmiao@gmail.com>
2012-06-15 17:50:09 +0800

    enabled gzip

--- bin/start.sh
+++ bin/start.sh
@@ -10,6 +10,8 @@ mkdir -p $NGINX_FILES
 mkdir -p $NGINX_FILES"/conf"
 mkdir -p $NGINX_FILES"/logs"
 
+cp $PWD"/conf/mime.types" $NGINX_FILES"/conf/"
+
 cat $PWD/conf/nginx.conf | sed -e "s|__MOOCHINE_HOME_VALUE__|$MOOCHINE_HOME|"\
                          | sed -e "s|__MOOCHINE_APP_VALUE__|$PWD|"\
                          > $NGINX_FILES/conf/p-nginx.conf 
