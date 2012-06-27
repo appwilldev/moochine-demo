5f70a6ecbf1c40bcb39eb63ea4ebf19ec14c6bb3
Demiao Lin <ldmiao@gmail.com>
2012-06-27 10:28:55 +0800

    minor changes

--- bin/debug.sh
+++ bin/debug.sh
@@ -1,7 +1,12 @@
 PWD=`pwd`
 NGINX_FILES=$PWD"/nginx_runtime"
-echo "" > $NGINX_FILES/logs/error.log 
-./bin/stop.sh ; 
-./bin/start.sh ;  
+
+./bin/stop.sh
+
+echo "" > $NGINX_FILES/logs/error.log
+
+./bin/start.sh
+
+echo "tail -f $NGINX_FILES/logs/error.log"
 tail -f $NGINX_FILES/logs/error.log
 
