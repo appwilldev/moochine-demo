7d381b5338efdeefddf2337fe3cfce0e6a6bb738
Demiao Lin <ldmiao@gmail.com>
2012-08-14 18:51:21 +0800

    minor changes

--- bin/generate_nginx_conf.sh
+++ bin/generate_nginx_conf.sh
@@ -7,16 +7,13 @@
 PWD=`pwd`
 
 NGINX_FILES=$PWD"/nginx_runtime"
-APP_NAME="moochine-demo"
 
-mkdir -p $NGINX_FILES
 mkdir -p $NGINX_FILES"/conf"
 mkdir -p $NGINX_FILES"/logs"
 
 cp $PWD"/conf/mime.types" $NGINX_FILES"/conf/"
 
-cat $PWD/conf/nginx.conf | sed -e "s|__MOOCHINE_HOME_VALUE__|$MOOCHINE_HOME|"\
-                         | sed -e "s|__MOOCHINE_APP_PATH_VALUE__|$PWD|"\
-                         | sed -e "s|__MOOCHINE_APP_NAME_VALUE__|$APP_NAME|"\
-                         > $NGINX_FILES/conf/p-nginx.conf 
+sed -e "s|__MOOCHINE_HOME_VALUE__|$MOOCHINE_HOME|" \
+    -e "s|__MOOCHINE_APP_PATH_VALUE__|$PWD|" \
+    $PWD/conf/nginx.conf > $NGINX_FILES/conf/p-nginx.conf 
 
