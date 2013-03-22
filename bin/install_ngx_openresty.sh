451ea057ddbd9348ce1967d1fd23e0e2d041449e
Demiao Lin <ldmiao@gmail.com>
2013-03-21 22:23:19 -0700

    added install_ngx_openresty.sh

---
+++ bin/install_ngx_openresty.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+mkdir ~/openresty_downloads
+cd ~/openresty_downloads
+
+apt-get install build-essential libreadline-dev libncurses5-dev libpcre3-dev libssl-dev perl libyaml-dev libmagickcore-dev libmagickwand-dev
+
+wget http://agentzh.org/misc/nginx/drizzle7-2011.07.21.tar.gz
+tar xzvf drizzle7-2011.07.21.tar.gz
+cd drizzle7-2011.07.21/
+./configure --without-server
+make libdrizzle-1.0
+make install-libdrizzle-1.0
+
+cd ..
+
+wget http://ftp.postgresql.org/pub/source/v9.2.3/postgresql-9.2.3.tar.gz
+tar vfxz postgresql-9.2.3.tar.gz
+cd postgresql-9.2.3
+./configure
+make
+make install
+
+cd ..
+
+wget http://agentzh.org/misc/nginx/ngx_openresty-1.2.7.1.tar.gz
+tar xzvf ngx_openresty-1.2.7.1.tar.gz
+cd ngx_openresty-1.2.7.1
+./configure --with-http_stub_status_module --with-http_realip_module --with-pcre-jit --with-luajit --with-http_postgres_module --with-http_drizzle_module --with-libpq=/usr/local/pgsql -j6
+make
+make install
+
+cd ..
+
+git clone git://github.com/ldmiao/yaml.git
+cd yaml
+make -f Makefile.linux_openresty
+make -f Makefile.linux_openresty install
+
+cd ..
+
+git clone git://github.com/azurewang/lua-resty-postgres.git
+cd lua-resty-postgres
+cp lib/resty/postgres.lua /usr/local/openresty/lualib/resty/
+
+cd ..
+
+git clone git://github.com/ldmiao/lua-cmsgpack.git
+cd lua-cmsgpack
+make -f Makefile.linux_openresty
+make -f Makefile.linux_openresty install
+
+cd ..
+
+git clone git://github.com/ldmiao/lua-zlib.git
+cd lua-zlib
+make linux
+make install
+
+cd ..
+
+git clone git://github.com/ldmiao/luafilesystem.git
+cd luafilesystem
+make
+make install
+
+cd ..
+
+git clone git://github.com/leafo/magick.git
+cd magick
+cp magick.lua /usr/local/openresty/lualib/
+cp -r magick /usr/local/openresty/lualib/
+
+cd ..
+
