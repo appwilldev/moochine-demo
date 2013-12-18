d83e07d965b4ca27256a482d24b23762f98c9d86
Demiao Lin <ldmiao@gmail.com>
2013-12-18 19:46:12 +0800

    added curl python-setuptools

--- bin/install_ngx_openresty.sh
+++ bin/install_ngx_openresty.sh
@@ -8,7 +8,9 @@ echo "------------------------------------------------------------------------"
 echo "INSTALL Required Library..."
 echo "------------------------------------------------------------------------"
 
-apt-get install build-essential bzip2 zip libreadline-dev libncurses5-dev libpcre3-dev libssl-dev perl libyaml-dev libmagickcore-dev libmagickwand-dev libcloog-ppl0 libgeoip-dev git htop iotop tmux rcconf rlwrap strace vim curl python-setuptools
+apt-get install build-essential bzip2 zip libreadline-dev libncurses5-dev libpcre3-dev libssl-dev perl libyaml-dev libmagickcore-dev libmagickwand-dev libcloog-ppl0 libgeoip-dev git htop iotop tmux rcconf rlwrap strace vim curl python-dev python-setuptools
+
+easy_install pyyaml
 
 echo ""
 echo "------------------------------------------------------------------------"
