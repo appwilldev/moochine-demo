4ac9a680eac341f412400d955a8ed7350f6d8b8f
Demiao Lin <ldmiao@gmail.com>
2014-06-11 10:43:19 +0800

    openresty 1.7.0.1

--- bin/install_ngx_openresty.sh
+++ bin/install_ngx_openresty.sh
@@ -59,10 +59,10 @@ echo "------------------------------------------------------------------------"
 echo "INSTALL OpenResty..."
 echo "------------------------------------------------------------------------"
 
-rm -rf ngx_openresty-1.5.11.1.tar.gz ngx_openresty-1.5.11.1
-wget http://agentzh.org/misc/nginx/ngx_openresty-1.5.11.1.tar.gz
-tar xzvf ngx_openresty-1.5.11.1.tar.gz
-cd ngx_openresty-1.5.11.1
+rm -rf ngx_openresty-1.7.0.1.tar.gz ngx_openresty-1.7.0.1
+wget http://agentzh.org/misc/nginx/ngx_openresty-1.7.0.1.tar.gz
+tar xzvf ngx_openresty-1.7.0.1.tar.gz
+cd ngx_openresty-1.7.0.1
 ./configure --with-http_stub_status_module --with-http_realip_module --with-pcre-jit --with-luajit --with-http_postgres_module --with-http_drizzle_module --with-libpq=/usr/local/pgsql --with-http_geoip_module -j6
 make -j6
 make install
