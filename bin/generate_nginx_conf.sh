1e7963f2ab4246166e9a2e57f9524fc87994bd21
KDr2 <killy.draw@gmail.com>
2014-08-09 21:54:06 +0800

    fix moochine app path

--- bin/generate_nginx_conf.sh
+++ bin/generate_nginx_conf.sh
@@ -15,7 +15,7 @@ mkdir -p $NGINX_FILES"/logs"
 cp $APP_ROOT/conf/mime.types $NGINX_FILES/conf/
 
 sed -e "s|__MOOCHINE_HOME_VALUE__|$MOOCHINE_HOME|" \
-    -e "s|__MOOCHINE_APP_PATH_VALUE__|$PWD|" \
+    -e "s|__MOOCHINE_APP_PATH_VALUE__|$APP_ROOT|" \
     -e "s|__NGINX_USER__|$CURRENT_USER|" \
     -e "s|__NGINX_GRP__|$CURRENT_USER|" \
     $APP_ROOT/conf/nginx.conf > $NGINX_FILES/conf/p-nginx.conf
